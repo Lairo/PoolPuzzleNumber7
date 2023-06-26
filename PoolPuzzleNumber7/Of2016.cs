34e56ac39b5036e47a1028b9c65d042bfa749ea0
chajus1 <chajus1@googlemail.com>
2023-06-26 07:58:13 +0100

    k+e

--- PoolPuzzleNumber7/Of2016.cs
+++ PoolPuzzleNumber7/Of2016.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PoolPuzzleNumber7
+﻿namespace PoolPuzzleNumber7
 {
     class Of2016 : Clowns
-    {        
+    {
         public override string Face { get { return "Of2016"; } }
     }
 }
